36548220d221a0394942141a0b9b0fb156add215
unknown <rahulkumar14061995@gmail.com>
2019-07-20 10:26:08 +0530

    Added New Folder'

---
+++ AngularProject/firstProject/src/app/test/test.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `<h2>{{"Hello "+ name}}</h2>`,
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+  // @Input()public parentData;
+    // or
+    @Input('parentData')public name;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
